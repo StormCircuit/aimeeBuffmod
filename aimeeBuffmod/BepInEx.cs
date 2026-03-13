456e1b8724b92aec3e35cda6edcb2ffd9fd6abf5
Storm <tjaccardi98@gmail.com>
2026-03-13 19:36:07 -0400

    first commit

---
+++ aimeeBuffmod/BepInEx.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace aimeeBuffmod
+{
+    #region BepInEx
+    [BepInEx.BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class aimeeBuffmod : BepInEx.BaseUnityPlugin
+    {
+        public const string pluginGuid = "com.username.aimeeBuffmod";
+        public const string pluginName = "aimeeBuffmod";
+        public const string pluginVersion = "1.0";
+        public static void Log(string line)
+        {
+            Debug.Log("[" + pluginName + "]: " + line);
+        }
+        void Awake()
+        {
+            try
+            {
+                var harmony = new Harmony(pluginGuid);
+                harmony.PatchAll();
+                Log("Patch succeeded");
+
+            }
+            catch (Exception e)
+            {
+
+                Log("Patch Failed");
+                Log(e.ToString());
+            }
+        }
+    }
+    #endregion
+}
---
+++ aimeeBuffmod/Patches/aimeeBuffModPatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Assets.Scripts.Objects;
+
+namespace aimeeBuffmod.Patches
+{
+  internal class aimeeBuffModPatch
+  {
+    [HarmonyPatch(typeof(RobotMining), "Awake")]
+    [HarmonyPostfix]
+    static void RobotMiningAwakePostfix(RobotMining __instance)
+    {
+      __instance.MaxSpeed = 6f; // Double the speed from 3f to 6f
+      __instance.WeatherDamageScale = 0.5f; // Halve the storm damage from 1f to 0.5f
+    }
+  }
+}
