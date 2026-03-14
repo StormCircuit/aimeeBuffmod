282bc93cf8e5a199538e0e41ed26d51263c7acfe
Storm <tjaccardi98@gmail.com>
2026-03-13 22:11:37 -0400

    Successful build adjusted values as per ingame testing

--- aimeeBuffmod/Patches/aimeeBuffModPatch.cs
+++ aimeeBuffmod/Patches/aimeeBuffModPatch.cs
@@ -8,14 +8,14 @@ using Assets.Scripts.Objects;
 
 namespace aimeeBuffmod.Patches
 {
-  internal class aimeeBuffModPatch
+  [HarmonyPatch]internal class aimeeBuffModPatch
   {
     [HarmonyPatch(typeof(RobotMining), "Awake")]
     [HarmonyPostfix]
     static void RobotMiningAwakePostfix(RobotMining __instance)
     {
-      __instance.MaxSpeed = 6f; // Double the speed from 3f to 6f
-      __instance.WeatherDamageScale = 0.5f; // Halve the storm damage from 1f to 0.5f
+      __instance.MaxSpeed = 2.6f; // Increase max speed
+      __instance.WeatherDamageScale = 0.25f; // Reduce weather damage
     }
   }
 }
